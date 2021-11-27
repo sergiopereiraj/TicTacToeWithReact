364a09d4350e8482727f97823fed275ceb63fd1d
sergiopereiraj <sergiopereira.jorquera@gmail.com>
2021-11-27 16:35:24 +0000

    guardado

---
+++ src/js/component/Titulo.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import "./TicTacToe.css";
+
+const Titulo = () => {
+	return (
+		<div className="container">
+			<h1>Tic Tac Toe Baby </h1>
+		</div>
+	);
+};
+
+export default Titulo;
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -3,11 +3,13 @@ import React from "react";
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 import TicTacToe from "./TicTacToe.jsx";
+import Titulo from "./Titulo.jsx";
 
 //create your first component
 const Home = () => {
 	return (
 		<div className="App">
+			<Titulo />
 			<TicTacToe />
 		</div>
 	);
